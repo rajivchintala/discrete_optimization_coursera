441b6381687f0621a22efa8f6f3d292e87bdf607
rajiv <rajiv.chintala@gmail.com>
2017-01-05 23:53:43 +0530

    Debugging Assignment 2
    Debugging Assignment 2

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -27,11 +27,11 @@ def solve_it(input_data):
     weight = 0
     taken = [0]*len(items)
 
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
+    # for item in items:
+    #     if weight + item.weight <= capacity:
+    #         taken[item.index] = 1
+    #         value += item.value
+    #         weight += item.weight
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
